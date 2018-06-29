8210c53657b21661b104089ae5a598361c395efd
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-06-29 18:54:54 +0800

    update next

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+// layout
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <html>
+                <Head>
+                    <meta key="X-UA-Compatible" http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+                    <meta key="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" name="viewport" />
+                    <meta key="apple-mobile-web-app-capable" content="yes" name="apple-mobile-web-app-capable" />
+                    <meta key="apple-mobile-web-app-status-bar-style" content="black" name="apple-mobile-web-app-status-bar-style" />
+                    <meta key="telephone-format-detection" content="telephone=no" name="format-detection" />
+                    <meta key="email-format-detection" content="email=no" name="format-detection" />
+                    <link rel="stylesheet" href="//webresource.c-ctrip.com/styles/h5/common/n_main.css" />
+                    <title>Hello World</title>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        )
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import Head from 'next/head'
-import { Component, Fragment } from 'react';
+import { Component, Fragment } from 'react'
 
 class IndexPage extends Component {
+    // 服务端同构数据出口
+    static async getInitialProps({ req }) {
+        const fetch = () => new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve('gogogo');
+            }, 300);
+        })
+        let rst = await fetch();
+        return { title: rst };
+    }
     constructor(props) {
         super(props);
         this.state = {
@@ -10,17 +20,18 @@ class IndexPage extends Component {
         }
     }
     render() {
+        const { title } = this.props;
         const { logoUrl, logoNm } = this.state;
         return (
             <Fragment>
                 <Head>
-                    <title>Thanos</title>
+                    <title>{title}</title>
                 </Head>
                 <div>Hello Next</div>
                 <div className="th__logo">
                     <img src={logoUrl} alt={logoNm}/>
                 </div>
-                <style jsx>{`
+                <style>{`
                     .th__logo {
                         width: 50px;
                         height: 50px;

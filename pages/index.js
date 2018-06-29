2c8bfd91d93318599385ed6eda75e01f9fd34fd8
xjq熊剑桥 <jqxiong@Ctrip.com>
2018-06-29 16:44:44 +0800

    Head

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,39 @@
-const IndexPage = () => <div>Hello Next</div>
+import Head from 'next/head'
+import { Component, Fragment } from 'react';
+
+class IndexPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            logoUrl: '/static/thanos.jpg',
+            logoNm: 'thanos'
+        }
+    }
+    render() {
+        const { logoUrl, logoNm } = this.state;
+        return (
+            <Fragment>
+                <Head>
+                    <title>Thanos</title>
+                </Head>
+                <div>Hello Next</div>
+                <div className="th__logo">
+                    <img src={logoUrl} alt={logoNm}/>
+                </div>
+                <style jsx>{`
+                    .th__logo {
+                        width: 50px;
+                        height: 50px;
+                        border-radius: 50%;
+                        overflow: hidden;
+                    }
+                    .th__logo img {
+                        width: 100%;
+                    }
+                `}</style>
+            </Fragment>
+        )
+    }
+}
 
 export default IndexPage
